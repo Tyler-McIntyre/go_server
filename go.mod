44836c6ca0cf0387dfb0d502cbbc573debf6f78c
Tyler-McIntyre <tmcintyre802@gmail.com>
2024-03-08 11:22:28 -0500

    Add add routing logic & pkgs

--- go.mod
+++ go.mod
@@ -3,3 +3,9 @@ module github.com/tyler-mcintyre/go_server
 go 1.22.1
 
 require github.com/go-chi/chi v1.5.5
+
+require github.com/tyler-mcintyre/go_client v0.0.0-20240308150007-11cb3c85787d
+
+require github.com/tyler-mcintyre/go_service_layer v0.0.0-20240308153919-d0dbc9859c7c
+
+require github.com/tyler-mcintyre/go_router v0.0.0-20240308161359-ce0a06e79418
--- server.go
+++ server.go
@@ -6,10 +6,22 @@ import (
 	"time"
 
 	"github.com/go-chi/chi"
+	current_weather "github.com/tyler-mcintyre/go_client/clients/current-weather"
+	handlers "github.com/tyler-mcintyre/go_router/api/handlers"
+	api "github.com/tyler-mcintyre/go_service_layer/api"
 )
 
 func BuildServer() (*http.Server, error) {
 	router := chi.NewRouter()
+	currentWeatherClient, err := current_weather.NewCurrentWeatherClient()
+	if err != nil {
+		return nil, err
+	}
+
+	service := api.NewService(&currentWeatherClient)
+
+	apiRouter := handlers.NewRouter(router, service)
+	apiRouter.AddRoutes()
 
 	port := "8080"
 	server := &http.Server{
